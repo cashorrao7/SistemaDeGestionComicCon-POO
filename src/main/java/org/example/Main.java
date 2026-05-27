f7138a09a141936bd2efb116d77ffa7a976d48d6
Jufro303 <j.rivas11@ufromail.cl>
2026-05-27 19:49:21 -0400

    Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,51 @@
 package org.example;
-
-
+public class Main {
+    import java.util.ArrayList;
+import java.util.List;
 public class Main {
     public static void main(String[] args) {
-        // 1. Instancias de objetos
-        // Deben de cambiar estos objetos, creen los propios suyos, estos estan de referencia.
+        Categoria catManga = new Categoria("manga y anime", 50, 9);
+        Categoria catStarWars = new Categoria("star wars", 30, 8);
+        Categoria catIndie = new Categoria("desarrollo de videojuegos indie", 20, 10);
 
-        Categoria animeCat = new Categoria("Manga y Anime", 50, 9.5);
-        Categoria arteCat = new Categoria("Ilustración Digital", 30, 8.0);
+        Cosplayer cosplayer1 = new Cosplayer(
+                "sopas", "chile", 24,
+                3, EstiloCosplay.ANIME
+        );
+        Artista artista1 = new Artista(
+                "pepe", "argentina", 28,
+                "libro cualquiera", 5
+        );
+        cosplayer1.agregarCategoria(catManga);
+        cosplayer1.agregarCategoria(catStarWars);
+        artista1.agregarCategoria(catManga);
+        artista1.agregarCategoria(catIndie);
+        Agrupacion clubFans = new Agrupacion("legion papu", catStarWars);
+        clubFans.agregarPersona(cosplayer1);
 
-        Cosplayer cosplayer1 = new Cosplayer("Luis", "Chile", 20, 5, EstiloCosplay.ANIME);
-        Artista artista1 = new Artista("Camila", "México", 22, "Wacom Intuos", 3);
+        System.out.println("\ninfo cosplayer");
+        cosplayer1.mostrarInformacion();
+        System.out.println("\ninfo artista");
+        artista1.mostrarInformacion();
+        System.out.println("\ninfo agrupacion");
+        clubFans.mostrarInformacion();
+        List<Interactivable> expositoresInteractivos = new ArrayList<>();
+        expositoresInteractivos.add(cosplayer1);
+        expositoresInteractivos.add(artista1);
 
-        Agrupacion comunidadAnime = new Agrupacion("Team Otaku UFRO", animeCat);
-        Panel panelPrincipal = new Panel(animeCat, "15:30 hrs");
-
-        // --- SECCIÓN DE PRUEBAS ---
-        // Ej: agregar personas a categorías, meter miembros a la agrupación, montar el panel y llamar a mostrarInformacion().
-
+        for (Interactivable expositor : expositoresInteractivos) {
+            System.out.println("ejecutando presentación interactiva:");
+            expositor.realizarPresentacion();
+            System.out.println();
+        }
+        Panel panelDebate = new Panel(catManga, "15:30 hrs");
+        panelDebate.agregarPanelista(artista1);
+        panelDebate.agregarPanelista(clubFans);
+        System.out.println("\ninfo del panel");
+        panelDebate.mostrarInformacion();
+        panelDebate.eliminarPanelista(artista1);
+        panelDebate.eliminarPanelista(clubFans);
+        System.out.println("panel cancelado, relaciones de asistencia eliminadas con exito");
 
     }
 }
